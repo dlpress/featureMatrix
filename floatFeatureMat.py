bf07a9c3a59e3daafdd0a1bb2e43c3a45ad565e8
dlpress <davidpress@Davids-MacBook-Pro.local>
2014-03-10 15:23:35 -0700

    added floatFeatureMat.py

--- floatFeatureMat.py
+++ floatFeatureMat.py
@@ -1,7 +1,11 @@
+"""Charges: output a file containing matrix index pairs.
+"""
+
 # Author: Dave Press
+#import codecs
 import numpy as np
 
-class BinaryFeatureMat(object):	
+class FloatFeatureMat(object):	
 	def __init__(self, rowNameIdx = [], colNameIdx = [], matrix = [], noNewCols = False, noNewRows = False):
 		""""""
 		self.rowNameIdx = rowNameIdx
@@ -18,9 +22,8 @@ class BinaryFeatureMat(object):
 		self.noNewCols = noNewCols
 		self.noNewRows = noNewRows
 			
-	def add(self, rowName, colName):
+	def add(self, rowName, colName, val=1.0):
 		""""""
-
 		if (self.rowNameMap.get(rowName) != None or not self.noNewRows) and (self.colNameMap.get(colName) != None or not self.noNewCols):
 			if self.rowNameMap.get(rowName) == None:
 				self.rowNameIdx.append(rowName)
@@ -30,11 +33,12 @@ class BinaryFeatureMat(object):
 				self.colNameIdx.append(colName)
 				self.colNameMap[colName] = self.numCols
 				self.numCols += 1
-			matrixPair = [self.rowNameMap[rowName], self.colNameMap[colName]]
-			self.matrix.append(matrixPair)
+			matrixTriple = [self.rowNameMap[rowName], self.colNameMap[colName], np.float32(val)]
+			self.matrix.append(matrixTriple)
 						 
 
-	def addFile(self, fileName, skip_lines = 1, rowNameInd = 0, colNameInd = 1, delimiter = '|'):
+	def addFile(self, fileName, skip_lines = 1, rowNameInd = 0, colNameInd = 1,
+				 valInd = 2, delimiter = '|', binaryFeatures = []):
 		""""""
 		file = open(fileName, 'r')
 		for l in range(skip_lines):
@@ -44,14 +48,22 @@ class BinaryFeatureMat(object):
 			if line == '':
 				break
 			tokens = line.split('\n')[0].split(delimiter)
-			self.add(tokens[rowNameInd], tokens[colNameInd])
+			if binaryFeatures == []:
+				if len(tokens)==3:
+					binaryFeatures = False
+				else:
+					binaryFeatures = True
+			if binaryFeatures:	
+				self.add(tokens[rowNameInd], tokens[colNameInd], 1.0)
+			else:
+				self.add(tokens[rowNameInd], tokens[colNameInd], tokens[valInd])
 		file.close()
 		
 	def toDenseMatrix(self):
 		"""Parse the sql table"""
-		denseMat = np.zeros((self.numRows,self.numCols),dtype=np.int8)
+		denseMat = np.zeros((self.numRows,self.numCols),dtype=np.float32)
 		for rc in self.matrix:
-			denseMat[rc[0],rc[1]] = 1
+			denseMat[rc[0],rc[1]] = rc[2]
 		return denseMat
 		
 
